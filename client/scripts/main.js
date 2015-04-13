b6b0ca5adbe0c80e6098a073125c10ec585f3a04
Viengo <viengooleg@gmail.com>
2015-04-13 21:24:58 +0400

    create new project

---
+++ client/scripts/main.js
@@ -0,0 +1,62 @@
+//Navigate menu
+
+$(document).ready(function () {
+    'use strict';
+    $(".fixed-menu, .menu-navigator").click(function () {
+        $(".menu-navigator").fadeToggle(500);
+        });
+    $("a").click(function () {
+        $(".fixed-menu").fadeToggle(500);
+    });
+    });
+
+
+
+//DT Switcher
+
+    $(document).ready(function () {
+        'use strict';
+        $("#1-dt").click(function(){
+            $(".second, .third").hide();
+        });
+
+        $("#1-dt").click(function(){
+            $(".first").show();
+        });
+        $("#2-dt").click(function(){
+            $(".first, .third").hide();
+        });
+
+        $("#2-dt").click(function(){
+            $(".second").show();
+        });
+        $("#3-dt").click(function(){
+            $(".second, .first").hide();
+        });
+
+        $("#3-dt").click(function(){
+            $(".third").show();
+        });
+    });
+
+
+ //Mail
+    $(document).ready(function () {
+        'use strict';
+        $(".send-form").click(function () {
+            $(".send-mail").fadeToggle("slow");
+        });
+        $("#exit").click(function () {
+            $(".send-mail").fadeToggle(800);
+        });
+
+        $('.view').click(function(){
+            var id = $('#profile-1');
+            $('#profile-1').fadeToggle('slow');
+        });
+        $('#close').click(function(){
+            $('#profile-1').fadeToggle('slow');
+        });
+    });
+
+
